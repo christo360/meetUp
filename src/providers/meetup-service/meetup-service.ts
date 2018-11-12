c6bc100bec87b5c6e920e7d26adddd38002e2f22
Christo <christo.sheppard@gmail.com>
2018-11-12 21:09:17 +0200

    getCategories method added to the Meetup Service, along with the specified baseUrl and API Key

--- src/providers/meetup-service/meetup-service.ts
+++ src/providers/meetup-service/meetup-service.ts
@@ -1,17 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the MeetupServiceProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class MeetupServiceProvider {
 
+  private baseUrl ='https://api.meetup.com';
+  private apiKey = '4458554c4b22ad705541556f762b7c'
+
   constructor(public http: HttpClient) {
-    console.log('Hello MeetupServiceProvider Provider');
+
+  }
+
+  getCategories() {
+    return new Promise(resolve => {
+      let headers = new HttpHeaders();
+      headers.append("Content-Type", "application/json");
+
+      this.http.get(this.baseUrl+'/2/categories?key='+this.apiKey).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
   }
 
 }
